14292cc99849799abf328813a5bf194490e2cb1c
Russ Miles <russ@russmiles.com>
2017-01-19 16:20:46 -0500

    Initial commit from the atomist bot

---
+++ src/main/java/com/russmiles/DerivedEventsServiceApplication.java
@@ -0,0 +1,12 @@
+package com.russmiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DerivedEventsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DerivedEventsServiceApplication.class, args);
+    }
+}
